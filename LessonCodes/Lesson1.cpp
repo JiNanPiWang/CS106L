f9dd64a6f531bf5c75b7cc094749728c6c099e01
G3 <purep@qq.com>
2024-02-08 20:33:41 +0800

    开始学习stringstream。如何创建，向流中添加内容，改变添加的位置

---
+++ LessonCodes/Lesson1.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Samsara on 2024/2/8.
+// File name: Lesson1.cpp
+// Description: 
+//
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 输出流是指向输出设备（如屏幕、文件、打印机等）的数据流。
+    // 在C++中，输出流通常用于将数据发送到标准输出（通常是屏幕）或文件中。
+    // 使用了 ostringstream 对象 oss 来构建一个字符串流，
+    ostringstream oss("Ito En Green Tea ");
+    cout << oss.str() << endl;
+
+    // 通过 oss << ... 可以向流中添加内容
+    oss << 16.9 << " Ounce ";
+    cout << oss.str() << endl;
+
+    // 从尾部添加内容
+    oss.seekp(0, stringstream::end); // 等价于ostringstream oss("Ito En Green Tea ", stringstream::ate);
+    oss << "!";
+    cout << oss.str() << endl;
+
+    return 0;
+}
