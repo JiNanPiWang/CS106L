288f22091b4b0985611594f7f90bda95fbb0134f
G3 <purep@qq.com>
2024-02-11 17:15:00 +0800

    add assignment

---
+++ Assignments/MarriagePact/main.cpp
@@ -0,0 +1,30 @@
+/* 
+Assignment 2: Marriage Pact
+
+This assignment asks you to discover your one true love using containers and pointers.
+There are two parts: implement get_applicants and find_matches.
+
+Submit to Paperless by 11:59pm on 2/1/2024.
+*/
+
+#include <iostream>
+#include <string>
+#include <queue>
+#include <set>
+
+std::set<std::string> get_applicants(std::string filename) {
+    // TODO: Implement this function. Feel free to change std::set to std::unordered_set if you wish!
+
+}
+
+std::queue<std::string*> find_matches(std::set<std::string> &students) {
+    // TODO: Implement this function.
+}
+
+
+
+int main() {
+    // Your code goes here. Don't forget to print your true love!
+    
+    return 0;
+}
