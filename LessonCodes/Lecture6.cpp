8b6a9721c978888ed6e560a23b2c3dae633f8e9f
G3 <purep@qq.com>
2024-02-13 21:52:36 +0800

    流实现getTypeValue

--- LessonCodes/Lecture6.cpp
+++ LessonCodes/Lecture6.cpp
@@ -4,19 +4,34 @@
 // Description: 
 //
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
 int getTypeValue()
 {
-    // typeid(int).name() 调用回返回一个表示整型（int）的名称。具体返回值取决于编译器的实现
-    // 返回的类型名称(可能是i)通常是编译器内部使用的名称，并不一定是易于人类阅读的形式。
-    cout << "Enter a" << typeid(int).name() << ": ";
-    return 0;
+    while (true)
+    {
+        // typeid(int).name() 调用回返回一个表示整型（int）的名称。具体返回值取决于编译器的实现
+        // 返回的类型名称(可能是i)通常是编译器内部使用的名称，并不一定是易于人类阅读的形式。
+        cout << "Enter a" << typeid(int).name() << ": ";
+        string line;
+        int result;
+        char extra;
+        if (!getline(cin, line))
+            throw domain_error("Unable to get line from cin");
+
+        istringstream iss(line);
+        if (iss >> result && !(iss >> extra))
+            return result;
+
+        cout << "Invalid format" << endl;
+    }
 }
 
 int main()
 {
-    getTypeValue();
+    int a = getTypeValue();
+    cout << "a = " << a << endl;
     return 0;
 }
