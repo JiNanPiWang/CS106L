f780498114d3ec9220d61a2b29eb513c42f1b63c
G3 <purep@qq.com>
2024-02-13 16:18:31 +0800

    std::find

--- LessonCodes/Lecture5.cpp
+++ LessonCodes/Lecture5.cpp
@@ -3,16 +3,23 @@
 // File name: Lecture5.cpp
 // Description: 
 //
-#include <map>
+#include <vector>
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    multimap<int, int> mp;
-    mp.insert(make_pair(3, 3));
-    mp.insert({3, 12});
-    cout << mp.count(3) << endl;
+    vector<int> vec{5, 4, 3, 2, 1};
+    for (auto x: vec) {
+        cout << x << " ";
+    }
+    cout << endl;
+    // Find the first occurrence of a value in a sequence.
+    auto it = find(vec.begin(), vec.end(), 5);
+    // *it是找到位置对应的值，而不是位置序号
+    cout << *it << endl;
+
     return 0;
 }
