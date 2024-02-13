990e64a7b5b160f9f30a255a57728b4ac55b813d
G3 <purep@qq.com>
2024-02-14 06:50:42 +0800

    template实现getTypeValue

--- LessonCodes/Lecture6.cpp
+++ LessonCodes/Lecture6.cpp
@@ -8,15 +8,16 @@
 
 using namespace std;
 
-int getTypeValue()
+template<typename T>
+T getTypeValue()
 {
     while (true)
     {
         // typeid(int).name() 调用回返回一个表示整型（int）的名称。具体返回值取决于编译器的实现
         // 返回的类型名称(可能是i)通常是编译器内部使用的名称，并不一定是易于人类阅读的形式。
-        cout << "Enter a" << typeid(int).name() << ": ";
+        cout << "Enter num" << typeid(T).name() << ": ";
         string line;
-        int result;
+        T result;
         char extra;
         if (!getline(cin, line))
             throw domain_error("Unable to get line from cin");
@@ -31,7 +32,9 @@ int getTypeValue()
 
 int main()
 {
-    int a = getTypeValue();
+    int a = getTypeValue<int>();
     cout << "a = " << a << endl;
+    double b = getTypeValue<double>();
+    cout << "b = " << b << endl;
     return 0;
 }
