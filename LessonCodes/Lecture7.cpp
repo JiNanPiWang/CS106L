99e8e4994dbcee151f464d8e8bf19ff9edb6b24e
G3 <purep@qq.com>
2024-02-21 20:36:43 +0800

    C++11的条件检查器，起谓词的作用

--- LessonCodes/Lecture7.cpp
+++ LessonCodes/Lecture7.cpp
@@ -8,19 +8,40 @@
 
 using namespace std;
 
-template <typename Collection, typename DataType>
-int countOccurences(const Collection &list, DataType val)
+// 修改模板函数以包含一个函数对象作为谓词
+template <typename Collection, typename UnaryPredicate>
+int countOccurences(const Collection &list, UnaryPredicate pred)
 {
     int count = 0;
     for (size_t i = 0; i < list.size(); ++i)
-        if (list[i] == val)
+        if (pred(list[i]))
             ++count;
     return count;
 }
 
+class GreaterThan
+{
+public:
+    explicit GreaterThan(int limit) : limit(limit) {}
+
+    bool operator() (int val) const
+    {
+        return val >= limit;
+    }
+
+private:
+    int limit;
+};
+
 int main()
 {
-    vector<int> v{1, 2, 3, 4};
-    cout << countOccurences<vector<int>, int>(v, 4) << endl;
+    vector<int> v{1, 2, 3, 4, 5, 6};
+    int limit = 4; // 创建一个GreaterThan对象，设置限制值为4
+    GreaterThan func(limit);
+
+    // 调用countOccurences，传入vector和GreaterThan对象
+    // 注意：这里不再需要明确指定模板参数，编译器可以自动推断
+    cout << countOccurences<vector<int>>(v, func)
+            << "item is greater than " << limit;
     return 0;
 }
