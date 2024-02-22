445317330b896ff31405b182ffa5d8b25be0308c
G3 <purep@qq.com>
2024-02-22 15:41:26 +0800

    nth_element找第n大元素，一般O(n)

--- LessonCodes/Lecture8.cpp
+++ LessonCodes/Lecture8.cpp
@@ -13,14 +13,22 @@ using namespace std;
 
 int main()
 {
-    vector<int> v{3, 2, 1};
+    vector<int> v{3, 2, 1, 6, 5, 4, 9, 8, 7};
 //    auto cmp = [](const int &a, const int &b) { return a > b; };
 //    auto [min, max] = minmax_element(v.begin(), v.end(), cmp);
 //    cout << *min << " " << *max << endl;
 
     // 第三个参数代表初始值
-    auto sum = accumulate(v.begin(), v.end(), 1);
-    cout << "sum: " << sum << endl;
+//    auto add = [](int sum, int a) { return sum + 666; };
+//    auto sum = accumulate(v.begin(), v.end(), 0, add);
+//    cout << "sum: " << sum << endl;
+
+    // STL的nth_element是一个非常有用的算法，它用于重新排列元素，
+    // 以便第n个位置的元素（假如所有元素都经过排序）会出现在其应有的位置上，
+    // 而且它前面的所有元素都不大于它，它后面的所有元素都不小于它。
+    nth_element(v.begin(), v.begin() + 3, v.end());
+    // 输出调整后位于第4位置的元素（索引为3，因为开始于0）
+    cout << "The fourth element is " << v[3] << '\n';
 
     return 0;
 }
