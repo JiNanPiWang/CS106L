237e18f30a627d86ef8399449485fa73af32921a
G3 <purep@qq.com>
2024-02-22 14:21:15 +0800

    accumulate计算总和

--- LessonCodes/Lecture8.cpp
+++ LessonCodes/Lecture8.cpp
@@ -5,14 +5,22 @@
 //
 #include <iostream>
 #include <vector>
+#include <deque>
+#include <algorithm>
+#include <numeric>
 
 using namespace std;
 
 int main()
 {
-    vector<int> v{3, 2, 1, 4, 5, 6};
-    auto cmp = [](const int &a, const int &b) { return a > b; };
-    auto [min, max] = minmax_element(v.begin(), v.end(), cmp);
-    cout << *min << " " << *max << endl;
+    vector<int> v{3, 2, 1};
+//    auto cmp = [](const int &a, const int &b) { return a > b; };
+//    auto [min, max] = minmax_element(v.begin(), v.end(), cmp);
+//    cout << *min << " " << *max << endl;
+
+    // 第三个参数代表初始值
+    auto sum = accumulate(v.begin(), v.end(), 1);
+    cout << "sum: " << sum << endl;
+
     return 0;
 }
