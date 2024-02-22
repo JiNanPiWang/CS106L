deed24b7d8c1da4744661c6e0deb6616ad6593a3
G3 <purep@qq.com>
2024-02-22 17:11:42 +0800

    使用copy_if和back_inserter将谓词表达的内容复制到其他的容器中

--- LessonCodes/Lecture8.cpp
+++ LessonCodes/Lecture8.cpp
@@ -26,9 +26,17 @@ int main()
     // STL的nth_element是一个非常有用的算法，它用于重新排列元素，
     // 以便第n个位置的元素（假如所有元素都经过排序）会出现在其应有的位置上，
     // 而且它前面的所有元素都不大于它，它后面的所有元素都不小于它。
-    nth_element(v.begin(), v.begin() + 3, v.end());
+//    nth_element(v.begin(), v.begin() + 3, v.end());
     // 输出调整后位于第4位置的元素（索引为3，因为开始于0）
-    cout << "The fourth element is " << v[3] << '\n';
+//    cout << "The fourth element is " << v[3] << '\n';
+
+    vector<int> v1;
+    copy_if(v.begin(), v.end(), back_inserter(v1),
+            [](int item){ return item >= 4; });
+
+    for (auto item: v1) {
+        cout << item << " ";
+    }
 
     return 0;
 }
