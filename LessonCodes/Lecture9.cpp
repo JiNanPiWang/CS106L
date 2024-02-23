1e960818ded26ae726fa906edf2b691ed52d777c
G3 <purep@qq.com>
2024-02-23 19:56:55 +0800

    迭代器的const

--- LessonCodes/Lecture9.cpp
+++ LessonCodes/Lecture9.cpp
@@ -4,6 +4,7 @@
 // Description: 
 //
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -13,16 +14,23 @@ int main()
 
     // 指针是常量
     int *const p = a;
-
-    // 指针指向的值是常量
+    // 指针指向的值是常量，同int const *p1 = a;
     const int *p1 = a;
-    int const *p2 = a;
+
 
     // p++会报错，(*p)++不会
     (*p)++;
-
     // p1++不会报错，(*p1)++会
     p1++;
+
+    vector<int> v = {1, 2, 3, 4};
+    const vector<int>::iterator it = v.begin();
+    vector<int>::const_iterator it1 = v.begin();
+
+    // it++会报错，(*it)++不会
+    (*it)++;
+    // it1++不会报错，(*it1)++会
+    it1++;
 
     return 0;
 }
