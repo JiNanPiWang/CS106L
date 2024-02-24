860b7cd498d67fdeb6ee481d07b183b6b85a9612
G3 <purep@qq.com>
2024-02-24 18:25:03 +0800

    initializer_list初始化StringVector

--- LessonCodes/Lecture11.cpp
+++ LessonCodes/Lecture11.cpp
@@ -9,11 +9,30 @@
 
 using namespace std;
 
+class StringVector
+{
+    vector<string> vec;
+
+public:
+    // 初始化列表
+    StringVector(const initializer_list<string> &vec) : vec(vec) {}
+
+};
+
 int main()
 {
-    vector<string> v{"Hello", "World"};
-    cout << v[0] << endl; // 等同于cout.operator<<(v.operator[](0));
-    v[1] += '!'; // 等同于v.operator[](1).operator+=("!");
+//    vector<string> v{"Hello", "World"};
+//    cout << v[0] << endl;
+    // 等同于
+    // cout.operator<<(v.operator[](0));
+    // operator<<(cout, v.operator[](0));
+
+//    v[1] += '!';
+    // 等同于
+    // v.operator[](1).operator+=("!");
+    // operator+=(operator[](v, 1), "!");
+
+    StringVector v = {"abc", "def"};
 
     return 0;
 }
