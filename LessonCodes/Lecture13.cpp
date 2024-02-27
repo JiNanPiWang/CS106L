d94a4d854caf6232147f4070387a422bfba2802b
sys-win11 <purep@qq.com>
2024-02-27 16:37:25 +0800

    左右值引用分辨

--- LessonCodes/Lecture13.cpp
+++ LessonCodes/Lecture13.cpp
@@ -15,12 +15,13 @@ int main()
     int *ptr = 0x02248837;
     vector<int> v1{1, 2, 3};
 
-    auto v4 = v1 + v2; // v1 is l, v2 is l, but v1 + v2 is rvalue
-    v1 += v4;
-    size_t size = v.size();
-    val = static_cast<int> (size);
-    v1[1] = 4 * i;
-    ptr = &val; // 右值，val的地址没有地址
-    v1[2] = *ptr;
+    auto &ptr2 = ptr;
+    auto &&v4 = v1 + v2; // 右值引用，可以理解为延长了右值的生命
+    auto &ptr3 = &val;
+
+    auto &&val2 = val; // 错误，不能将左值绑定给右值引用
+
+    const auto &ptr3 = ptr + 5; // const可以
+
     return 0;
 }
