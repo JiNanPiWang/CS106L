785b3641c2f68aaa0704e03a2b3786803ff01c95
sys-win11 <purep@qq.com>
2024-03-04 16:02:56 +0800

    类的继承访问权限

--- LessonCodes/Lecture15.cpp
+++ LessonCodes/Lecture15.cpp
@@ -15,7 +15,10 @@ public:
 
     explicit Drink(std::string flavor) : _flavor((std::move(flavor))) {}
 
-    virtual void make() = 0;
+    virtual void make()
+    {
+        cout << "Made tea from Drink class!" << endl;
+    }
 
     virtual ~Drink() = default;
 
@@ -23,7 +26,9 @@ private:
     std::string _flavor;
 };
 
-class Tea : Drink
+// 使用 public 继承时，基类的 public 成员在派生类中仍然是 public，protected 成员在派生类中变成 protected，而 private 成员在派生类中仍然是 private。
+// 派生类对象可以直接访问基类的 public 和 protected 成员，但不能直接访问基类的 private 成员。其余同理
+class Tea : public Drink
 {
 public:
     Tea() = default;
@@ -35,13 +40,17 @@ public:
     void make()
     override
     {
-        cout << "Made tea from Drink class!" << endl;
+        cout << "Made tea from Tea class!" << endl;
     }
 };
 
 int main()
 {
     Tea t("red");
+
+    // 使用make()和Drink::make()
     t.make();
+    t.Drink::make();
+
     return 0;
 }
