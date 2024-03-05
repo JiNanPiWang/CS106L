e48bd86b7ad38ea1047ec804ebe6cda234960d3d
sys-win11 <purep@qq.com>
2024-03-05 21:24:45 +0800

    main函数主线程，会直接结束程序，没运行完的线程也会被析构，并调用std::terminate

--- LessonCodes/Lecture17.cpp
+++ LessonCodes/Lecture17.cpp
@@ -4,7 +4,7 @@
 // Description: 
 //
 #include <iostream>
-#include <utility>
+#include <chrono>
 #include <fstream>
 #include <mutex>
 #include <thread>
@@ -14,6 +14,12 @@ using std::cout, std::endl;
 
 void greet(int id)
 {
+    // 加入延时，可以更好的发现问题
+    // 程序中确实创建了两个线程，主线程（main）会继续执行它下面的代码，不会等待 thread1 和 thread2 完成。
+    // 如果关联的线程还在运行且没有被 detach，那么 std::thread 的析构函数会调用 std::terminate
+    // 析构函数通过结合性(joinable)来判断是否需要调用 std::terminate
+    // TODO: 看joinable如何实现
+    std::this_thread::sleep_for(std::chrono::seconds(4));
     cout << "Hello there! My name is " << id << endl;
 }
 
