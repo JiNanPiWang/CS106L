846273e81b65795bdfd9328edff9e80c1893937c
sys-win11 <purep@qq.com>
2024-03-05 10:26:14 +0800

    RAII例子

--- LessonCodes/Lecture16.cpp
+++ LessonCodes/Lecture16.cpp
@@ -1,6 +1,26 @@
 //
 // Created by Samsara on 2024/3/5.
 // File name: Lecture16.cpp
-// Description: 
+// Description: RAII
 //
 // noexcept应该在构造或者析构函数中出现的原因：比如在析构的一半抛出异常，会导致内存泄漏
+
+#include <iostream>
+#include <fstream>
+
+// 不符合RAII，也就是资源获取即初始化
+void printFile()
+{
+    std::ifstream input;
+    input.open("a.txt");
+    // ...
+    input.close();
+}
+
+// 符合RAII
+void printFileRAII()
+{
+    std::ifstream input("a.txt");
+    // ...
+    // 会自动close
+}
