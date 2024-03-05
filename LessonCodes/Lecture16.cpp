8fbf7e2e22b557cc12c4039c66b37806403cdf48
sys-win11 <purep@qq.com>
2024-03-05 11:24:24 +0800

    RAII，锁的例子

--- LessonCodes/Lecture16.cpp
+++ LessonCodes/Lecture16.cpp
@@ -7,6 +7,8 @@
 
 #include <iostream>
 #include <fstream>
+#include <mutex>
+#include <map>
 
 // 不符合RAII，也就是资源获取即初始化
 void printFile()
@@ -23,4 +25,20 @@ void printFileRAII()
     std::ifstream input("a.txt");
     // ...
     // 会自动close
+}
+
+// 不符合RAII
+void cleanDatabase(std::mutex &databaseLock, std::map<int, int> &database)
+{
+    databaseLock.lock();
+    // ...
+    databaseLock.unlock();
+}
+
+// 符合RAII
+void cleanDatabaseRAII(std::mutex &databaseLock, std::map<int, int> &database)
+{
+    // 详细看源码，std::lock_guard<std::mutex>是类型，可以理解为vector<int>，但其似乎没有成员函数
+    std::lock_guard<std::mutex> Lock(databaseLock);
+    // ...
 }
