2657d71c78aa7573459e017a22366b1396e27c8b
sys-win11 <purep@qq.com>
2024-03-06 10:47:39 +0800

    可以看出main也是一个线程，与其他并行

--- LessonCodes/Lecture17.cpp
+++ LessonCodes/Lecture17.cpp
@@ -19,17 +19,22 @@ void greet(int id)
     // 如果关联的线程还在运行且没有被 detach，那么 std::thread 的析构函数会调用 std::terminate
     // 析构函数通过结合性(joinable)来判断是否需要调用 std::terminate
     // TODO: 看joinable如何实现
-    std::this_thread::sleep_for(std::chrono::seconds(4));
-    cout << "Hello there! My name is " << id << endl;
+    cout << "Hello there! My name is " << std::this_thread::get_id() << endl;
 }
 
 int main()
 {
     cout << "Greetings from my threads..." << endl;
 
+    // 可以看出main也是一个线程，与其他并行
+    cout << "Hello there! My name is " << std::this_thread::get_id() << endl;
+
     // 可以理解为，告诉一个工人需要去做什么
     std::thread thread1(greet, 1);
     std::thread thread2(greet, 2);
+
+    thread1.join();
+    thread2.join();
 
     cout << "All greetings done!" << endl;
     return 0;
