7ebc3d2fc319b48efcf94206c899fea58d228c4a
sys-win11 <purep@qq.com>
2024-03-07 09:31:35 +0800

    写完get_applicants

--- Assignments/Assignment2_marriagepact/main.cpp
+++ Assignments/Assignment2_marriagepact/main.cpp
@@ -11,10 +11,18 @@ Submit to Paperless by 11:59pm on 2/1/2024.
 #include <string>
 #include <queue>
 #include <set>
+#include <fstream>
 
 std::set<std::string> get_applicants(std::string filename) {
-    // TODO: Implement this function. Feel free to change std::set to std::unordered_set if you wish!
-
+    std::set<std::string> st;
+    std::ifstream inputFile(filename);
+    if (inputFile.is_open()) {
+        std::string line;
+        while (std::getline(inputFile, line)) {
+            st.insert(line);
+        }
+    }
+    return st;
 }
 
 std::queue<std::string*> find_matches(std::set<std::string> &students) {
@@ -25,6 +33,7 @@ std::queue<std::string*> find_matches(std::set<std::string> &students) {
 
 int main() {
     // Your code goes here. Don't forget to print your true love!
-    
+    auto x=  get_applicants(
+            R"(D:\Projects\2024-2-26-CS106L\Assignments\Assignment2_marriagepact\students.txt)");
     return 0;
 }
