914e2ffd9455e47dcc677e1c640702cc8154f092
sys-win11 <purep@qq.com>
2024-03-08 11:43:52 +0800

    完成输出结果功能

--- Assignments/Assignment4_weatherforecast/main.cpp
+++ Assignments/Assignment4_weatherforecast/main.cpp
@@ -13,6 +13,8 @@ Submit to Paperless by 11:59pm on 2/16/2024.
 #include <concepts>
 #include <algorithm>
 #include <numeric>
+#include <fstream>
+#include <iomanip>
 
 
 // write convert_f_to_c function here. Remember it must be a template function that always returns
@@ -50,9 +52,20 @@ int main()
             {40, 45, 50, 55, 60}
     };
 
-    // Convert temperatures to Celsius and output to output.txt
+    std::string output_path = R"(D:\Projects\2024-2-26-CS106L\Assignments\Assignment4_weatherforecast\output.txt)";
+    std::ofstream outputFile(output_path);
 
-    
+    // Convert temperatures to Celsius and output to output.txt
+    outputFile << "Temperatures to Celsius: " << std::endl;
+    for (const auto &row: readings)
+    {
+        // 保留两位小鼠输出
+        for (const auto &f_temp: row)
+            outputFile << std::fixed << std::setprecision(2) << convert_f_to_c(f_temp) << " ";
+        outputFile << '\n';
+    }
+    outputFile << '\n';
+
     // Find the maximum temperature for each day and output to output.txt
     // 使用匿名函数和stl中的函数来完成
     auto maximum = get_forecast(readings,
@@ -60,6 +73,10 @@ int main()
                                 {
                                     return *std::max_element(wea.begin(), wea.end());
                                 });
+    outputFile << "Maximum temperature for each day: " << std::endl;
+    for (const auto &max_temp: maximum)
+        outputFile << max_temp << " ";
+    outputFile << "\n\n";
 
     // Find the minimum temperature for each day and output to output.txt
     auto minimum = get_forecast(readings,
@@ -67,6 +84,10 @@ int main()
                                 {
                                     return *std::min_element(wea.begin(), wea.end());
                                 });
+    outputFile << "Minimum temperature for each day: " << std::endl;
+    for (const auto &min_temp: minimum)
+        outputFile << min_temp << " ";
+    outputFile << "\n\n";
 
     // Find the average temperature for each day and output to output.txt
     auto average = get_forecast(readings,
@@ -75,6 +96,10 @@ int main()
                                     return std::accumulate(wea.begin(), wea.end(), double(0)) /
                                            static_cast<double>(wea.size());
                                 });
+    outputFile << "Average temperature for each day: " << std::endl;
+    for (const auto &avg_temp: average)
+        outputFile << avg_temp << " ";
+    outputFile << "\n\n";
 
     return 0;
 }
